62063c2fd8ad96ecff96df74e93fdab91c226b29
nazmulhuda1 <nazmulhuda3130@gmail.com>
2023-08-05 21:50:11 +0600

    slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,3 +38,15 @@ function toggle() {
     if (this.closest('.has-child').classList != 'expand');
     this.closest('.has-child').classList.toggle('expand');
 }
+
+// swiper slide
+const swiper = new Swiper('.swiper', {
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+
+});
